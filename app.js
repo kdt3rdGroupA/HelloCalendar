88e5bf6c1d0e4f271153b6aeb9ab8cc8f594bfa7
gitjaeran <devjaeran@gmail.com>
2022-11-18 15:41:42 +0900

    feat: layout

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const http = require('http').Server(app);
 
 const PORT = 8000;
 
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use('/static', express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // app.listen(PORT, (req, res) => {
 //   console.log(`http://localhost:${PORT}`);

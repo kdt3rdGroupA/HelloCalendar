48a838cd611b3a678bf767b535aa9ae9c23f71fa
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2022-11-18 16:06:15 +0900

    test: 로그인, 회원가입,패스워드암호화 테스트파일에서 개발됨

---
+++ models/Login.js
@@ -0,0 +1,38 @@
+// 유저의 로그인정보 테이블("user_login_key")접근
+const Login = (Sequelize, DataTypes) => {
+  const model = Sequelize.define(
+    'user_login_key',
+    {
+      id : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        primaryKey : true,
+        autoIncrement : true
+      },
+      userid : {
+        type : DataTypes.STRING(20),
+        allowNull : false
+      },
+      name : {
+        type : DataTypes.STRING(20),
+        allowNull : false
+      },
+      salt : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+      },
+      hash_pw : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+      },
+    },
+    {
+      tableName : 'user_login_key',
+      freezeTableName : true,
+      timestamps : false
+    }
+  );
+  return model;
+};
+
+module.exports = Login;

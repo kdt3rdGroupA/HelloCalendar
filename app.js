4dfb10e62eaa8629e02e60d7ea7896084d112ffc
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2022-11-19 15:54:57 +0900

    feat: 404페이지 추가

--- app.js
+++ app.js
@@ -10,18 +10,29 @@ app.use('/views', express.static(__dirname + '/views'));
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({
+  secret : "\(^Д^)/\(^Д^)/\(^Д^)/\(^Д^)/",
+  resave : false,
+  saveUninitialized : true
+}));
 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// login, signup 요청과 각 페이지 렌더
 const loginRouter = require('./routes/login');
-app.post('/login', loginRouter);
+app.use('/login', loginRouter);
 
 // app.listen(PORT, (req, res) => {
 //   console.log(`http://localhost:${PORT}`);
 // });
+
+// 404 Page
+app.get('*', (req,res) => {
+  res.render('404');
+});
 
 http.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);

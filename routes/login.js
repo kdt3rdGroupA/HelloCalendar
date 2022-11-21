a54acc1eef197a8566bc1f95cfe4b3d7849b7a5a
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2022-11-21 10:43:36 +0900

    feat: 이메일 인증, 비밀번호 찾기, 변경 추가

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,12 @@ router.get('/', controller.loginPage);
 router.get('/signup', controller.signupPage);
 // 회원가입 페이지 렌더
 
+router.get('/pwreset', controller.pwResetPage);
+// 패스워드 재발급 페이지
+
+router.get('/pwchange', controller.pwChangePage);
+// 패스워드 변경 페이지
+
 router.post('/', controller.login);
 // 로그인 요청
 
@@ -18,5 +24,17 @@ router.post('/signup', controller.signup);
 
 router.post('/logout', controller.logout);
 // 로그아웃요청
+
+router.post('/emailAuth', controller.emailAuth);
+// 이메일 인증
+
+router.post('/idcheck', controller.idCheck);
+// 아이디 중복 검사
+
+router.post('/pwreset', controller.pwReset);
+// 비밀번호 재설정
+
+router.post('/pwchange', controller.pwChange);
+// 비밀번호 변경
 
 module.exports = router;

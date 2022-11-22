f1a5f8028a8c6ce2a13a18f9935b720f9f9dba65
gitjaeran <devjaeran@gmail.com>
2022-11-22 16:26:06 +0900

    feat: 구글로그인 이후 index페이지로 이동

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,5 @@
+const { render } = require("ejs");
+
 function login() {
   const form_login = document.forms["form_login"];
   const id = document.querySelector("#id");
@@ -25,6 +27,7 @@ function login() {
 
 
 //Google Login API
+//로그인 정보를 가져오기 위한 handleCredentialResponse 함수 생성
 function handleCredentialResponse(response) {
   // decodeJwtResponse() is a custom function defined by you
   // to decode the credential response.
@@ -35,8 +38,18 @@ function handleCredentialResponse(response) {
   console.log('Given Name: ' + responsePayload.given_name);
   console.log('Family Name: ' + responsePayload.family_name);
   console.log("Image URL: " + responsePayload.picture);
-  console.log("Email: " + responsePayload.email); 
-};
+  console.log("Email: " + responsePayload.email);
+
+  //index로 이동
+  window.location = '/';
+
+
+  //****구현해야하는 기능****
+  //구글 로그인 성공하여 index 페이지로 올 경우,로그인(class= "headerLogin")이 유저 프로필(class="gUserProfile")로 바뀜
+  //유저 프로필은 구글 프로필 이미지(static/js/login.js ->   responsePayload.picture)
+  };
+
+//로그인 정보를 변환하기 위한 parseJWT 함수 생성
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
@@ -46,3 +59,5 @@ function parseJwt (token) {
 
     return JSON.parse(jsonPayload);
 };
+
+

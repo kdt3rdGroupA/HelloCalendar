8876c350a160ecf9a69a1eb1260dadd32c7828c5
Sujeong <nksj98@naver.com>
2022-11-24 15:56:03 +0900

    fix:날짜 변경시 일정 삭제 안되는 오류 수정

--- app.js
+++ app.js
@@ -1,61 +1,63 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
 const PORT = 8001;
 
-app.set('view engine', 'ejs');
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/static', express.static(__dirname + '/static'));
-app.use(express.urlencoded({ extended: true })); // body-parser post 요청 할 때 body로 받게해줌 
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true })); // body-parser post 요청 할 때 body로 받게해줌
 app.use(express.json());
-app.use(session({
-  secret : "\(^Д^)/\(^Д^)/\(^Д^)/\(^Д^)/",
-  resave : false,
-  saveUninitialized : true
-}));
+app.use(
+  session({
+    secret: "(^Д^)/(^Д^)/(^Д^)/(^Д^)/",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.get('/', (req, res) => {
-  console.log(typeof(req.session.isLogin));
+app.get("/", (req, res) => {
+  console.log(typeof req.session.isLogin);
   if (req.session.isLogin == true) {
     console.log(1);
-    res.render('index', {
-      isLogin : true,
-      name : req.session.data.name,
-      email : req.session.data.email
-      });
+    res.render("index", {
+      isLogin: true,
+      name: req.session.data.name,
+      email: req.session.data.email,
+    });
   } else {
     console.log(2);
-    res.render('index', {isLogin : false});
+    res.render("index", { isLogin: false });
   }
 });
 
 // login과 관련된 인증들
-const loginRouter = require('./routes/login');
-app.use('/login', loginRouter);
+const loginRouter = require("./routes/login");
+app.use("/login", loginRouter);
 
 // todo 관련
-const todoRouter = require('./routes/todo');
-app.use('/todo', todoRouter); 
+const todoRouter = require("./routes/todo");
+app.use("/todo", todoRouter);
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-app.use(bodyParser());			// 요청 본문 파싱
+app.use(bodyParser.json());
+app.use(bodyParser()); // 요청 본문 파싱
 
 // manual 페이지 새창에 랜더
-app.get('/manual', (req, res) => {
-  res.render('manual');
+app.get("/manual", (req, res) => {
+  res.render("manual");
   // res.render('manual', { title: 'id님 환영' });
 });
 
 // 404 Page
-app.get('*', (req,res) => {
-  res.render('404');
+app.get("*", (req, res) => {
+  res.render("404");
 });
 
 http.listen(PORT, () => {

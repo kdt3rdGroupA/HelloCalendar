b88b371a528691a31507c5602414639da5690d72
choi0105 <chleo77@naver.com>
2022-11-28 14:49:28 +0900

    모델 Todo(맞는거)

--- models/todo.js
+++ models/todo.js
@@ -2,13 +2,13 @@ const Todo = (Sequelize, DataTypes) => {
   const model = Sequelize.define(
     "todo",
     {
-      id : {
+      id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
-      key_id : {
+      key_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
@@ -16,24 +16,38 @@ const Todo = (Sequelize, DataTypes) => {
         type: DataTypes.STRING(50)
       },
       priority : {
-        type: DataTypes.STRING(10)
+        type: DataTypes.INTEGER
+      },
+      startline: {
+        type: DataTypes.STRING(20)
       },
       deadline: {
         type: DataTypes.STRING(20)
+      },
+      complete: {
+        type: DataTypes.BOOLEAN
+      },
+      business: {
+        type: DataTypes.BOOLEAN
       }
     }, {
       tableName: 'todo',
+
       freezeTableName: true,
       timestamps: false,
-      indexes: [{
-        name: "key_id",
-        using: 'BTREE',
-        fields: [{
+      indexes: [
+        {
           name: "key_id",
-          collate: 'utf8_general_ci',
-          order: 'DESC'
-        }]
-      }]
+          using: "BTREE",
+          fields: [
+            {
+              name: "key_id",
+              collate: "utf8_general_ci",
+              order: "DESC",
+            },
+          ],
+        },
+      ],
     }
   );
   return model;

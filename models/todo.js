5cbdd77cd3237a79124b5d1b7d6772d20de497b2
HS-1000 <68994620+HS-1000@users.noreply.github.com>
2023-03-24 14:39:42 +0900

    FIX: 코드 청소

--- models/todo.js
+++ models/todo.js
@@ -2,40 +2,38 @@ const Todo = (Sequelize, DataTypes) => {
   const model = Sequelize.define(
     "todo",
     {
-      id: {
+      id : {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
       },
-      key_id: {
+      key_id : {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      task: {
-        type: DataTypes.STRING(50),
+      text : {
+        type: DataTypes.STRING(50)
       },
-      priority: {
-        type: DataTypes.STRING(10),
+      priority : {
+        type: DataTypes.STRING(10)
       },
-    },
-    {
-      tableName: "todo",
+      deadline: {
+        type: DataTypes.STRING(20)
+      }
+    }, {
+      tableName: 'todo',
       freezeTableName: true,
       timestamps: false,
-      indexes: [
-        {
+      indexes: [{
+        name: "key_id",
+        using: 'BTREE',
+        fields: [{
           name: "key_id",
-          using: "BTREE",
-          fields: [
-            {
-              name: "key_id",
-              collate: "utf8_general_ci",
-              order: "DESC",
-            },
-          ],
-        },
-      ],
+          collate: 'utf8_general_ci',
+          order: 'DESC'
+        }]
+      }]
     }
   );
   return model;
